c71fb7f87e07e0d5ca96e39c702a0a855b59bd18
Jaedeee <limjh-wm20@student.tarc.edu.my>
2021-07-27 19:53:32 +0800

    27 July 2021

---
+++ ComputerGamePractical/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public int score = 0;
+
+    public void changeScore(int gemValue)
+    {
+        score += gemValue;
+        text.text = score.ToString();
+
+    }
+}
